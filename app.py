b1eda14ab5f375322ccaa5cd62bf5207e5bf7eb1
Mohamad Samardeh Haghighi Fard <samardehmohamad@gmail.com>
2024-04-19 20:15:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,27 @@
+import asyncio
 from aiogram import executor
 import filters
 import handlers
 from loader import dp
 
+async def handle_client(reader, writer):
+    data = await reader.read(100)
+    message = data.decode()
+    addr = writer.get_extra_info('peername')
+
+    print(f"Received {message} from {addr}")
+
+    writer.close()
+
+async def main():
+    server = await asyncio.start_server(
+        handle_client, '0.0.0.0', 8000)
+
+    addr = server.sockets[0].getsockname()
+    print(f'Serving on {addr}')
+
+    async with server:
+        await server.serve_forever()
+
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True, port=8000)
+    asyncio.run(main())
