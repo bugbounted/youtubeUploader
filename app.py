8782b377c4809011e52e58f9cf368d8bfd09535d
Mohamad Samardeh Haghighi Fard <samardehmohamad@gmail.com>
2024-04-19 19:52:50 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 from aiogram import executor
+from flask import Flask, request, jsonify
 
 import filters
 import handlers
 from loader import dp
 
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello, World!"
+
 if __name__ == '__main__':
-  executor.start_polling(dp)
+    executor.start_polling(dp)
+
+    # Run the Flask app on port 8000
+    app.run(host='0.0.0.0', port=8000)
